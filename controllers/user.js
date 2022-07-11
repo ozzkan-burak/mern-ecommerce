0c4aa1a24e4659261f57b79220675cf0872c0290
Burak ÖZKAN <burak.ozkan@flo.com.tr>
2022-07-11 23:53:27 +0300

    episode7

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,8 @@
+const User = require("../models/user");
+
 exports.signup = (req, res) => {
   console.log("req.body", req.body);
-  const user = new Usewr(req.body);
+  const user = new User(req.body);
 
   user.save((err, user)=> {
     if(err) {
