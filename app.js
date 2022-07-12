5fa832b29f66d50f378093e06314023c6063d1cd
Burak ÖZKAN <burak.ozkan@flo.com.tr>
2022-07-12 23:48:30 +0300

    episode8

--- app.js
+++ app.js
@@ -21,7 +21,9 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("DB Connected"));
+  .then(() => console.log("DB Connected"))
+  .catch((err)=> console.log(err));
+
 
 // routes middleware
 app.use("/api", userRoutes);
--- controllers/user.js
+++ controllers/user.js
@@ -1,14 +1,13 @@
 const User = require("../models/user");
 
 exports.signup = (req, res) => {
-  console.log("req.body", req.body);
+
   const user = new User(req.body);
-  console.log(user);
 
   user.save((err, user)=> {
     if(err) {
-      return res.satatus(400).json({
-        error
+      return res.status(400).json({
+        err
       })
     }
       res.json({
