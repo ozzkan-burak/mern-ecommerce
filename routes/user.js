9cbc0296d6689873f3ce71c8bb0ecf1c2a26222d
Burak ÖZKAN <burak.ozkan@flo.com.tr>
2022-07-21 22:50:07 +0300

    episode14

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,20 @@ const express = require("express");
 const router = express.Router();
 
 // controllers
-const { signup, signin, signuot } = require("../controllers/user");
+const {
+  signup,
+  signin,
+  signuot,
+  requireSignin,
+} = require("../controllers/user");
 const { userSignupValidator } = require("../validator");
 
 router.post("/signup", userSignupValidator, signup);
 router.post("/signin", signin);
 router.get("/signuot", signuot);
+
+router.get("/hello", requireSignin, (req, res) => {
+  res.send("selam millet");
+});
 
 module.exports = router;
